026bab5d7ac49693d33c71472b858e68d1b5d262
Berk <berkbeleli@gmail.com>
2023-04-24 23:36:20 +0300

    Adds the created Component into Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,21 @@
 import React, { useContext, useRef, useEffect } from "react";
+import { SidebarContext } from "../../contexts/SidebarContext";
+import useMediaQueryHook from "../../hooks/useMediaQueryHook";
+import {
+  SearchBox,
+  DateToday,
+  Logo,
+  HamburgerIcon,
+  CloseIcon,
+  UserIcon,
+  Navbar,
+} from "..";
+
 import styles from "./Header.module.css";
 
 export const Header = () => {
-
+	const { isSidebarOpen, setHeaderHeight } = useContext(SidebarContext);
+	const { isDesktopOrLaptop, isTabletOrMobile } = useMediaQueryHook();
   
 	const header = useRef(null);
   
@@ -10,13 +23,29 @@ export const Header = () => {
 	  // dynamically retrieve header_1 height to set sidebar absolute position below it
 	  if (header !== null) {
 		let height = header.current.clientHeight;
-
+		setHeaderHeight(height);
 	  }
 	}, [header]);
   
 	return (
 	  <header className={styles.header}>
-		
+		 <section ref={header} className={styles.header_1}>
+        {isDesktopOrLaptop && <SearchBox />}
+        {isTabletOrMobile && <HamburgerIcon />}
+        <Logo inHeader={true} />
+        {isTabletOrMobile && isSidebarOpen === false && <UserIcon />}
+        {isTabletOrMobile && isSidebarOpen && <CloseIcon />}
+        {isDesktopOrLaptop && <DateToday />}
+      </section>
+
+      <section
+        className={`${
+          isSidebarOpen === false || isDesktopOrLaptop ? styles.header_2 : ""
+        }`}
+      >
+        {isDesktopOrLaptop && <Navbar />}
+        {isTabletOrMobile && isSidebarOpen === false && <DateToday />}
+      </section>
 	  </header>
 	);
   };
