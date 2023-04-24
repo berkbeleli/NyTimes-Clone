638324b2dbe92af04d8f653cbb7001c5b41d295e
Berk <berkbeleli@gmail.com>
2023-04-24 23:32:43 +0300

    Adds Warning Icon Module

---
+++ src/components/warning-icon/WarningIcon.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { IoWarningOutline } from "react-icons/io5";
+import styles from "./WarningIcon.module.css";
+
+export const WarningIcon = () => {
+  return <IoWarningOutline className={styles.warning_icon} />;
+};
