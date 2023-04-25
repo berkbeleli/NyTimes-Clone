d4d5201041bf03b453f36c610e8320f03740eba1
Berk <berkbeleli@gmail.com>
2023-04-25 14:15:40 +0300

    Adds index.js for component folder

---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import { Header } from "./header/Header";
+import { Navbar } from "./navbar/Navbar";
+import { NavItems } from "./nav-items/NavItems";
+import { Sidebar } from "./sidebar/Sidebar";
+import { Logo } from "./logo/Logo";
+import { HamburgerIcon } from "./hamburger-icon/HamburgerIcon";
+import { CloseIcon } from "./close-icon/CloseIcon";
+import { UserIcon } from "./user-icon/UserIcon";
+import { WarningIcon } from "./warning-icon/WarningIcon";
+import { DateToday } from "./date/DateToday";
+import { SearchBox } from "./search-box/SearchBox";
+import { SearchForm } from "./search-form/SearchForm";
+import { SortingSelect } from "./sorting-select/SortingSelect";
+import { PaginationUI } from "./pagination/Pagination";
+import { SectionTitle } from "./section-title/SectionTitle";
+import { Article } from "./article/Article";
+import { Footer } from "./footer/Footer";
+import { FooterItemsRow1 } from "./footer-items/FooterItemsRow1";
+import { FooterItemsRow2 } from "./footer-items/FooterItemsRow2";
+import { Error } from "./error/Error";
+import { Loader } from "./loader/Loader";
+
+export {
+  Header,
+  Navbar,
+  NavItems,
+  Sidebar,
+  Logo,
+  HamburgerIcon,
+  CloseIcon,
+  UserIcon,
+  WarningIcon,
+  DateToday,
+  SearchBox,
+  SearchForm,
+  SortingSelect,
+  PaginationUI,
+  SectionTitle,
+  Article,
+  Footer,
+  FooterItemsRow1,
+  FooterItemsRow2,
+  Error,
+  Loader,
+};
