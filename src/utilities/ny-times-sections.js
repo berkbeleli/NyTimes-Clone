f3d69fa1a8255c4e80cc135d5432fc855068e6c9
Berk <berkbeleli@gmail.com>
2023-04-25 14:00:27 +0300

    Adds sections Datas

---
+++ src/utilities/ny-times-sections.js
@@ -0,0 +1,95 @@
+import { v4 as uuidv4 } from "uuid";
+
+//navbar sections data
+export const sectionsData = [
+  {
+    id: uuidv4(),
+    label: "World",
+    slug: "world",
+  },
+  {
+    id: uuidv4(),
+    label: "U.S.",
+    slug: "us",
+  },
+  {
+    id: uuidv4(),
+    label: "Politics",
+    slug: "politics",
+  },
+  {
+    id: uuidv4(),
+    label: "N.Y.",
+    slug: "nyregion",
+  },
+  {
+    id: uuidv4(),
+    label: "Business",
+    slug: "business",
+  },
+  {
+    id: uuidv4(),
+    label: "Opinion",
+    slug: "opinion",
+  },
+  {
+    id: uuidv4(),
+    label: "Tech",
+    slug: "technology",
+  },
+  {
+    id: uuidv4(),
+    label: "Science",
+    slug: "science",
+  },
+  {
+    id: uuidv4(),
+    label: "Health",
+    slug: "health",
+  },
+  {
+    id: uuidv4(),
+    label: "Sports",
+    slug: "sports",
+  },
+  {
+    id: uuidv4(),
+    label: "Arts",
+    slug: "arts",
+  },
+  {
+    id: uuidv4(),
+    label: "Books",
+    slug: "books",
+  },
+  {
+    id: uuidv4(),
+    label: "Style",
+    slug: "style",
+  },
+  {
+    id: uuidv4(),
+    label: "Food",
+    slug: "food",
+  },
+  {
+    id: uuidv4(),
+    label: "Travel",
+    slug: "travel",
+  },
+  {
+    id: uuidv4(),
+    label: "Magazine",
+    slug: "magazine",
+  },
+  {
+    id: uuidv4(),
+    label: "T Magazine",
+    slug: "t-magazine",
+  },
+  {
+    id: uuidv4(),
+    label: "Real Estate",
+    slug: "realestate",
+  },
+];
