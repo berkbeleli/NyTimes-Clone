9b6b802a73bdd4147d1f68fd22f1085dfc67e7ed
Berk <berkbeleli@gmail.com>
2023-04-25 14:22:45 +0300

    Adds index.js for Pages

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import { Home } from "./home/Home";
+
+import { SectionArticles } from "./section-articles/SectionArticles";
+import { QueryArticles } from "./query-articles/QueryArticles";
+import { ErrorPage } from "./error-page/ErrorPage";
+
+export { Home, SectionArticles, QueryArticles, ErrorPage };
